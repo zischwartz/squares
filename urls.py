eee7b8f56066c6e0bd530e30ab6502dd8c56345d
Zach Schwartz <zischwartz@gmail.com>
2011-11-23 16:55:43 -0500

    first commit, restructured

---
+++ urls.py
@@ -0,0 +1,31 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+import settings
+
+
+urlpatterns = patterns('',
+    (r'^$',  direct_to_template, {'template': 'index.html'}),
+    (r'^home/$',  direct_to_template, {'template': 'index.html'}),
+
+    # Examples:
+    # url(r'^$', 'squares.views.home', name='home'),
+    # url(r'^squares/', include('squares.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATICFILES_DIRS,
+        }),
+   )
