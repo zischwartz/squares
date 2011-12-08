4c2e892fa217cff08ce2dcb568cd7faf17949cc8
Fred Truman <ftruman@gmail.com>
2011-12-07 23:37:31 -0500

    added dance() and stopDancing() functions with accelerometer functionality

--- static/js/base.js
+++ static/js/base.js
@@ -14,7 +14,11 @@ var // DOM NodeList caches
 	lastCheckInName = 'a TEST location',
 	nearbyVenues = [],
 	speed = 300,
-	checkInDuration = 1; // how long check-ins last, in minutes
+	previousBeta,
+	previousGamma,
+	deltaBeta,
+	deltaGamma,
+	checkInDuration = 120; // how long check-ins last, in minutes
 	
 	$.fn.pause = function(duration) { //calling pause on jquery events 
 		$(this).animate({ dummy: 1 }, duration);
@@ -29,14 +33,15 @@ var // DOM NodeList caches
 		lastCheckInName = $(this).text();
 		checkIN($(this).attr('title'));
 	});
-	$("#back-to-desired a").live("click", function(){
+	$("#nearby-intro .back a").live("click", function(){
 		$('#requested-venue').pause(speed).fadeIn(speed);
 		$('.nearby-venues').fadeOut(speed);				
 		$('.nearby-intro').fadeOut(speed);				
 	});
-	$('a#makeHappy').live('click', function(){doActivity(5)});
-	$('a#makeSad').live('click', function(){doActivity(-5)});
-
+	$('a#makeHappy').live('click', function(){logActivity(5)});//I called this 'logActivity' cuz there's probably a function for doing the activity before the score is sent to the server
+	$('a#makeSad').live('click', function(){logActivity(-5)});
+	$('a#dance').live('click', function(){dance()});
+	
 
 	function init() { // start everything up and check if you've validated squares with foursquare
 		if( token.indexOf('=') != -1 ) { // does the current address have an equals sign in it?
@@ -69,11 +74,12 @@ var // DOM NodeList caches
 		navigator.geolocation.getCurrentPosition(function(loc){
 			var lat = loc.coords.latitude;
 			var lon = loc.coords.longitude;
-			var doStuff = "<p>Well, here we are at <strong>" + lastCheckInName + "</strong>, " + userName + ". Awesome!</p> <p>Now let's do some stuff.</p> <p><a id='makeHappy'> Make me happy</a><p> or <a id='makeSad'>Make me sad</a></p>";
+			var doStuff = "<div id='activities'><p>Well, here we are at <strong>" + lastCheckInName + "</strong>, " + userName + ". Awesome!</p> <p>Now let's do some stuff.</p></div>";
 			if ( checkedIn == false) {
 				findNearby(lat,lon); // take the lat lon values and look for nearby venues in the foursquare API
 			} else {
 				$(doStuff).hide().appendTo('#content').pause(speed).fadeIn(speed);
+				initActivities();
 			}
 		});
 	}// end getLocation()
@@ -130,7 +136,7 @@ var // DOM NodeList caches
 	}
 
 	function getMoreVenueOptions() {
-		$('<div class="nearby-intro"><p id="back-to-desired" class="nav"><a>x</a></p><p>Ok, so where would you rather go, Mr. Smartypants?</p></div>').hide().appendTo('#content').pause(speed).fadeIn(speed); //				
+		$('<div class="nearby-intro"><p class="back nav"><a>x</a></p><p>Ok, so where would you rather go, Mr. Smartypants?</p></div>').hide().appendTo('#content').pause(speed).fadeIn(speed); //				
 		$('<div/>', {
 			'class': 'nearby-venues',
 			html: nearbyVenues.join('')
@@ -183,6 +189,40 @@ var // DOM NodeList caches
 		}
 	} // end areYouCheckedIn()
 	
+	function initActivities() {
+		var activities = "<a id='makeHappy' class='activity'> Make me happy</a><a id='makeSad' class='activity'>Make me sad</a><a id='dance' class='activity'>Let\'s dance!</a>";
+		$(activities).appendTo("#activities").pause(speed).fadeIn(speed);
+	}
+	
+	function dance() {
+		console.log('we\'re dancing');
+		var nav = '<div class="activity-back"><p class="back nav"><a>x</a></p>';
+		$('div#activities').fadeOut(speed);
+		$(nav).hide().appendTo("#content").pause().fadeIn(speed);
+		window.addEventListener('deviceorientation', function(event) {
+			if (!event.gamma && !event.beta) {
+	          event.gamma = -(event.x * (180 / Math.PI));
+	          event.beta = -(event.y * (180 / Math.PI));
+	        }
+			var overThreshold = Math.abs(event.gamma) > 4 || Math.abs(event.beta) > 4;
+	        var gamma = overThreshold ? event.gamma : 0;
+	        var beta = overThreshold ? event.beta : 0;				
+			if (previousGamma != gamma || previousBeta != beta) {
+				var x = Math.round(4 * gamma);
+				var y = Math.round(4 * beta);			
+				$('#square').css('margin-left', -30 + x);
+				$('#shadow').css('margin-left', -60 + x);
+				$('#square').css('top', 60 + y);
+				$('#shadow').css('top', 140 + y);
+			}
+			previousGamma = gamma;
+			previousBeta = beta;
+		}, false);
+	}
+	
+	function stopDancing() {
+		window.removeEventListener('deviceorientation', function(){});
+	}	
 
 	//for our server	
 	function sendToServer(data){
@@ -194,8 +234,7 @@ var // DOM NodeList caches
 		});
 	} //end sendToServer
 
-
-	function doActivity(points){
+	function logActivity(points){
 		sendToServer({'type':'activity', 'points':points, 'id':checkinId});	
 	}
 
