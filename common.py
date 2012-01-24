b140dfbc8917e5b1b278c0c682d0ba7498a6e51c
Zach Schwartz <zischwartz@gmail.com>
2012-01-24 16:51:20 -0500

    did some scaling of my own, works fairly well, though we need to scale (or just decrease) outputs

--- common.py
+++ common.py
@@ -1,4 +1,17 @@
+# def scale(val, src, dst):
+#         """
+#         Scale the given value from the scale of src to the scale of dst.
+#         """
+#         return ((val - src[0]) / (src[1]-src[0])) * (dst[1]-dst[0]) + dst[0]
+# 
 
+from math import log
+
+def scale(val):
+    if val <=0:
+        return 0
+    else:
+        return (log(val)/10)
 
 
 def get_inputs(venue):
@@ -6,13 +19,12 @@ def get_inputs(venue):
 	inputs.append(venue['stats']['checkinsCount'])
 	inputs.append(venue['hereNow']['count'])
 	inputs.append(venue['stats']['tipCount'])
-	return(inputs)
+        
+        inputs = map(scale, inputs)        
+
+        # print 'INPUTS: ' 
+        # print inputs
+        # return(inputs)
 
 
-
-class sillystring(str):
-    def __init__(self):
-        self.content = ''
-    def write(self, content):
-        self.content = content
 
