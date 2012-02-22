a429fbdd87d81aea3e6596511f9c8d711cc7ccab
Fred Truman <ftruman@gmail.com>
2012-02-21 21:44:51 -0500

    finally got the routes working right. I think the cache problem with the venue name is fixed finally. Added in a checkin confirmation screen. Some styling, the dance activity.

--- static/js/base.js
+++ static/js/base.js
@@ -11,13 +11,12 @@
 		squareDimension = 120,
 		squarePixelDim = 10,
 		pixelCounter = 0,
-		checkInDuration = 1,
+		checkInDuration = 120,
 		userName = 'userName',
 		checkedIn = false,
 		userId ='',
 		checkinId ='', 
 		userPhoto='',
-		lastCheckInName = 'a TEST location',
 		nearbyVenues = [],
 		rankedVenues = [],
 		speed = 300,
@@ -29,6 +28,8 @@
 		desiredVenueAddress,
 		desiredVenueID
 		;
+		$.lastCheckInName = 'a TEST location';
+		
 	$('.square').live("click", function() {
 		console.log('click');
 	});
@@ -48,17 +49,40 @@
 	                    maxIndex = i;
 	        return {'index':maxIndex, 'value':this[maxIndex]};
 	}
+	//checkIN to venues flow
 	$("a.checkin").live("click", function(){
+		//show a checkin confirmation button
+		var text = $(this).children(":first").text();
+		$('.confirm .venue-name').html(text);
+		$('.confirm').show();
+		$('a.checkin-confirm').attr('title',$(this).attr('title'));		
+	});
+	$("a.checkin-confirm").live('click', function(){
+		$('.confirm').hide();
 		$('.loading').show();
+		console.log($(this).attr('title'));
 		checkIN($(this).attr('title'));
 	});
+	$("a.checkin-decline").live("click", function(){
+		$('.confirm').hide();
+	});
+	//
 	$("a.target-more_options").live("click", function(){
 		window.location.replace(home + '#access_token=' + accessToken + '/more_options');
 	});
 	$("a.target-suggest").live("click", function(){
 		window.location.replace(home + '#access_token=' + accessToken + '/suggest');
 	});
-	//NEED TO FIGURE OUT HOW CHECKOUT WORKS WITH REPOPULATING INFO
+	$("a.dance").live("click", function(){
+		window.location.replace(home + '#access_token=' + accessToken + '/dance');
+	});
+	$(".stop-dancing").live("click", function(){ //this is the <p> id to stop the eventLstener. the interface functionality is handled by the '.back a' click function above
+		stopDancing();
+		window.location.replace(home + '#access_token=' + accessToken + '/activities');
+	});
+	//NEED TO FIGURE OUT HOW CHECKOUT WORKS WITH REPOPULATING INFO, cuz there's a lastVenue
+	//cache that doesn't clear. The data doesn't cache (which is good) but the display
+	//name for the venue caches. CAN YOU TAKE A LOOK, ZACH??
 	$("a.target-check_out").live("click", function(){
 		checkOut();
 	});
@@ -74,7 +98,7 @@
 	}
 	function checkIN(venueID) {
 		$('.loading').show();
-		console.log('lastCheckInName at top of checkIn = ' + lastCheckInName);
+		console.log('lastCheckInName at top of checkIn = ' + $.lastCheckInName);
 		$.post('https://api.foursquare.com/v2/checkins/add?oauth_token=' + accessToken + '&broadcast=public&venueId=' + venueID , function(Cdata) {
 			console.log('checkin', Cdata);
 			$.get('https://api.foursquare.com/v2/venues/'+ venueID+ "?access_token=" + accessToken + "&client_id=" + CLIENTID + "&client_secret=" + CLIENTSECRET, function(Vdata){
@@ -82,12 +106,14 @@
 				// alert('Venue Data Loaded');	
 				sendToServer({type: 'checkin', 'userName': userName, 'userId':userId, 'checkinData': Cdata.response.checkin, 'venueData': Vdata.response.venue });
 				checkinId = Cdata.response.checkin.id;
-				lastCheckInName = Cdata.response.checkin.venue.name;
+				$.lastCheckInName = Cdata.response.checkin.venue.name;
+				console.log('response name is = ' + Cdata.response.checkin.venue.name);
+				console.log('response name from lastCheckInName = ' + $.lastCheckInName);
 				checkedIn = true;
 				getLocation();
 				}			
 			);
-			console.log('accessToken at bottom of checkIn = ' + accessToken);
+			console.log('lastCheckInName at top of checkIn = ' + $.lastCheckInName);
 			$('.loading').hide();
 			window.location.replace(home + '#access_token=' + accessToken + '/activities');
 		});
@@ -132,13 +158,16 @@
 						$.each(json.response.groups[0].items, function() {
 							var venueAddress = this.location.address;
 							if (venueAddress!=undefined) {venueAddress='<h3>' + venueAddress + '</h3>';}else{venueAddress='';}
-							nearbyVenues.push('<a class="checkin nearby" title="' + this.id + '"><h2>' + this.name + '</h2>' + venueAddress + '</a>'); 
+							nearbyVenues.push('<a class="checkin option" title="' + this.id + '"><h2>' + this.name + '</h2>' + venueAddress + '</a>');
+							//console.log('<a class="checkin option" title="' + this.id + '"><h2>' + this.name + '</h2>' + venueAddress + '</a>'); 
 						});
 					} //end /learn/choose success
 				}) //end learn choose ajax
 			}
 		});
-		$('.loading').hide();		
+		$('.loading').hide();
+		//add in functions from welcome HERE???	
+		//areYouCheckedIn();	
 		window.location.replace(home + '#access_token=' + accessToken + '/suggest');
 	}// end findNearby()
 	function getLocation() { // look at the GPS of the device and then call the API
@@ -168,8 +197,8 @@
 			success: function(json) {
 				console.log(json);
 				try{ // what's this? Fred would like to know more about 'try' and catch(err)
-					lastCheckInName = json.response.checkins.items[0].venue.name;
-					console.log('lastCheckInName = ' + lastCheckInName);
+					$.lastCheckInName = json.response.checkins.items[0].venue.name;
+					console.log('lastCheckInName = ' + $.lastCheckInName);
 					lastCheckInTime = json.response.checkins.items[0].createdAt; // 
 					elapsedTime = (currentTime - lastCheckInTime) / 60; // in minutes	
 					checkinId = json.response.checkins.items[0].id;
@@ -242,6 +271,45 @@
 		$('.square div').css('height',squareDimension/squarePixelDim);
 		$('.square div').css('width',squareDimension/squarePixelDim);
 		$('.loading').hide();
+	}
+	function dance() {
+		danceScore = 0;
+		console.log('we\'re dancing');
+		if (window.DeviceOrientationEvent) {
+		  console.log("DeviceOrientation is supported");
+		  window.addEventListener('deviceorientation', bustamove, false);
+		} else {
+			alert("Not supported on your device or browser. Sorry.");
+		}
+		
+	}
+	bustamove = function(eventData) { // this is the event handler for the device orientation in dance()
+        var LR = eventData.gamma;
+        var FB = eventData.beta;
+        var DIR = eventData.alpha;
+		var overThreshold = Math.abs(LR) > 4 || Math.abs(FB) > 4;
+        var gamma = overThreshold ? LR : 0;
+        var beta = overThreshold ? FB : 0;
+		if (previousGamma != gamma || previousBeta != beta) {
+			var x = Math.round(4 * gamma);
+			var y = Math.round(4 * beta);			
+			$('.square').css({'margin-left': (-squareDimension/2 + x),'margin-top': (y)});
+			$('.shadow').css({'margin-left': (-squareDimension + x), 'top': (300 + y)});
+		}
+		danceScore += (Math.abs(gamma) + Math.abs(beta))/10000;
+		console.log(danceScore);
+		previousGamma = gamma;
+		previousBeta = beta;
+	}
+	function stopDancing() { // remove the handler and log the score for the activity
+		window.removeEventListener('deviceorientation', bustamove, false);
+		logActivity(danceScore);
+		danceScore = 0;
+		//store the points someplace temporarily until it's time to log everything?
+		//or send the score now and store it in the DB?
+	}
+	function logActivity(points){
+		sendToServer({'type':'activity', 'points':points, 'id':checkinId});	
 	}
 	//this sets up routing with SAMMY.JS
     var squares = $.sammy('.content', function() {
@@ -329,7 +397,7 @@
 				context.app.swap('');
 				context.render('static/templates/activities.template', {
 					//load in any data parsed from the API here
-					venueName : lastCheckInName
+					venueName : $.lastCheckInName
 				}).then(function(element){
 					$('.loading').hide();
 					$(element).appendTo(context.$element()); //add it to the screen
@@ -337,6 +405,30 @@
 				});
 			}
         });
+	//DANCE ROUTE
+		this.get(/#access_token=[0-9A-Z]{48}\/dance/, function(context) {
+			//do we need to redirect them through the AUTHORIZED ROUTE?
+			//console.log('accessToken = ' + accessToken);
+			$('.loading').show();
+			var tempToken = ((window.location.href).split('=')[1]).split('/')[0];
+			if(accessToken === '') {
+				//find the window.location and get everything between '=' and '/'
+				//this is the accessToken. I know this seems redundant
+				window.location.replace(home + '#access_token=' + tempToken);
+			} else {		
+				var context = this;
+				context.app.swap('');
+				context.render('static/templates/dance.template', {
+					//load in any data parsed from the API here
+					userName : userName						
+				}).then(function(element){
+					$('.loading').hide();
+					$(element).appendTo(context.$element()); //add it to the screen
+					addPixels();
+					dance();
+				});
+			}
+        });		
 	//MORE_OPTIONS ROUTE
 		this.get(/#access_token=[0-9A-Z]{48}\/more_options/, function(context) {
 			//do we need to redirect them through the AUTHORIZED ROUTE?
@@ -353,7 +445,8 @@
 				context.render('static/templates/more_options.template', {
 					//load in any data parsed from the API here
 					venueName : desiredVenueName,
-					userName : userName
+					userName : userName,
+					venues : nearbyVenues.join('')
 				}).then(function(element){
 					$('.loading').hide();
 					$(element).appendTo(context.$element()); //add it to the screen
